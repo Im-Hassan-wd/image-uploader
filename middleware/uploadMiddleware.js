94ad50e3ea5c8aa00b240dee276d80f09004ae1e
Im-Hassan-wd <alhassansahih@gmail.com>
2022-03-27 00:29:07 +0100

    uploading file...

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,2 +1,12 @@
 const path = require('path');
 const multer = require('multer'); 
+
+const storage = multer.diskStorage({
+ destination: function(req, file, cb) {
+   cb(null, 'wick/uploads/');
+ },
+ filename: function(req, file, cb) {
+   let ext = path.extname(file.originalname);
+   cb(null, Date.now() + ext);
+ }
+});
